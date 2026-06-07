9e3e124a1241aa5246084b96865efda069d2896e
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 19:48:54 +0100

    Pedido de letra

--- jogo_forca.py
+++ jogo_forca.py
@@ -17,3 +17,17 @@ palavra_selecionada = random.choice(palavras).lower()
 
 letras = [" _ "] * len(palavra_selecionada)
 
+while " _ " in letras:
+    print(" ".join(letras))
+    
+    letra=input("Introduza uma letra:")
+    
+    if letra in palavra_selecionada:
+        for i in range(len(palavra_selecionada)):
+            if palavra_selecionada[i]==letra:
+                letras[i] = letra
+                
+    else:
+        print("Erro")
+print(f"Vitória!! A palavra era: {palavra_selecionada}")
+
