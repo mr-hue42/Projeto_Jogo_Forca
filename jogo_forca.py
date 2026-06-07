e281da64ac28da25a13354d3d98dc3612b4b0a5b
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 19:58:39 +0100

    Inicio e Rondas
    Definir o inicio do jogo e a realização de várias rondas

--- jogo_forca.py
+++ jogo_forca.py
@@ -13,37 +13,44 @@ import random
 palavras = ["Mesa","Cadeira","Livro","Copo","Mochila","Garrafa","Teclado",
             "Espelho", "Candeeiro","Tesoura"]
 
-palavra_selecionada = random.choice(palavras).lower()
+jogar=input("Queres jogar? (Escreva jogar para o jogo iniciar ou sair para abandonar o jogo): ").lower()
 
-letras = [" _ "] * len(palavra_selecionada)
+while jogar == "jogar":
 
-letras_usadas = [ ]
+    palavra_selecionada = random.choice(palavras).lower()
 
-erros = 0
+    letras = [" _ "] * len(palavra_selecionada)
 
-max_erros= 6
+    letras_usadas = [ ]
 
-while " _ " in letras and erros < max_erros:
-    print(" ".join(letras))
+    erros = 0
+
+    max_erros= 6
+
+    while " _ " in letras and erros < max_erros:
+        print(" ".join(letras))
     
-    letra=input("Introduza uma letra:")
+        letra=input("Introduza uma letra:")
     
-    if letra in letras_usadas:
-        print("Letra já utilizada")
-        continue
-    letras_usadas.append(letra)
-    print(letras_usadas)
+        if letra in letras_usadas:
+            print("Letra já utilizada")
+            continue
+        letras_usadas.append(letra)
+        print(letras_usadas)
     
-    if letra in palavra_selecionada:
-        for i in range(len(palavra_selecionada)):
-            if palavra_selecionada[i]==letra:
-                letras[i] = letra
+        if letra in palavra_selecionada:
+            for i in range(len(palavra_selecionada)):
+                if palavra_selecionada[i]==letra:
+                    letras[i] = letra
                 
+        else:
+            print("Letra errada")
+            erros += 1
+    if " _ " not in letras:
+        print(f"Vitória!! A palavra era: {palavra_selecionada}")
     else:
-        print("Letra errada")
-        erros += 1
-if " _ " not in letras:
-    print(f"Vitória!! A palavra era: {palavra_selecionada}")
-else:
-    print(f"Derrota!! A palavra era: {palavra_selecionada}")
+        print(f"Derrota!! A palavra era: {palavra_selecionada}")
+    jogar= input("Queres jogar novamente? (Escreva jogar para continuar a jogar): ")
+
+    print("Obrigado por jogares")
 
