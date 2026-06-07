4e89a3fad8b48fbc19a6638d4a26ffed4a5151fd
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 10:47:18 +0100

    Contagem de erros
    Adicionar ao programa a contabilização de erros. O nº max de erros são 6.
    Ao fim de 6 erros cometidos o jogador perde.

---
+++ testes/Contagem_erros.py
@@ -0,0 +1,39 @@
+palavras = ["Mesa","Cadeira","Livro","Copo","Mochila","Garrafa","Teclado",
+            "Espelho", "Candeeiro","Tesoura"]
+import random
+
+palavra_selecionada = random.choice(palavras).lower()
+
+print(palavra_selecionada)
+
+letras = [" _ "] * len(palavra_selecionada)
+
+letras_usadas = [ ]
+
+erros = 0
+
+max_erros= 6
+
+while " _ " in letras and erros < max_erros:
+    print(" ".join(letras))
+    
+    letra=input("Introduza uma letra:")
+    
+    if letra in letras_usadas:
+        print("Letra já utilizada")
+        continue
+    letras_usadas.append(letra)
+    print(letras_usadas)
+    
+    if letra in palavra_selecionada:
+        for i in range(len(palavra_selecionada)):
+            if palavra_selecionada[i]==letra:
+                letras[i] = letra
+                
+    else:
+        print("Letra errada")
+        erros += 1
+if " _ " not in letras:
+    print(f"Vitória!! A palavra era: {palavra_selecionada}")
+else:
+    print(f"Derrota!! A palavra era: {palavra_selecionada}")
