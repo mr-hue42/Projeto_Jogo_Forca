707fd8b440fb5370b2f3b0977c37aa97a4e0cd53
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 20:02:16 +0100

    Criar função para jogar

--- jogo_forca.py
+++ jogo_forca.py
@@ -1,56 +1,59 @@
-print("Bem vindo(a) ao jogo da forca")
-print()
-print("Regras:")
-print()
-print("-Cada traço representa uma letra da palavra secreta")
-print("-Em cada tentiva deverá indicar uma letra")
-print("-O número máximo de tentativas erradas é igual a 6")
-print("-Vitória: Descobrir a palavra secreta antes de atingir o nº máximo de erros")
-print("-Derrota: Atingir o nº máximo de erros (6) antes de descobrir a palavra secreta")
-
 import random
 
-palavras = ["Mesa","Cadeira","Livro","Copo","Mochila","Garrafa","Teclado",
-            "Espelho", "Candeeiro","Tesoura"]
+def jogar_forca():
+    print("Bem vindo(a) ao jogo da forca")
+    print()
+    print("Regras:")
+    print()
+    print("-Cada traço representa uma letra da palavra secreta")
+    print("-Em cada tentiva deverá indicar uma letra")
+    print("-O número máximo de tentativas erradas é igual a 6")
+    print("-Vitória: Descobrir a palavra secreta antes de atingir o nº máximo de erros")
+    print("-Derrota: Atingir o nº máximo de erros (6) antes de descobrir a palavra secreta")
 
-jogar=input("Queres jogar? (Escreva jogar para o jogo iniciar ou sair para abandonar o jogo): ").lower()
 
-while jogar == "jogar":
 
-    palavra_selecionada = random.choice(palavras).lower()
-
-    letras = [" _ "] * len(palavra_selecionada)
-
-    letras_usadas = [ ]
-
-    erros = 0
-
-    max_erros= 6
-
-    while " _ " in letras and erros < max_erros:
-        print(" ".join(letras))
+    palavras = ["Mesa","Cadeira","Livro","Copo","Mochila","Garrafa","Teclado",
+                "Espelho", "Candeeiro","Tesoura"]
     
-        letra=input("Introduza uma letra:")
+    jogar=input("Queres jogar? (Escreva jogar para o jogo iniciar ou sair para abandonar o jogo): ").lower()
     
-        if letra in letras_usadas:
-            print("Letra já utilizada")
-            continue
-        letras_usadas.append(letra)
-        print(letras_usadas)
+    while jogar == "jogar":
+
+        palavra_selecionada = random.choice(palavras).lower()
     
-        if letra in palavra_selecionada:
-            for i in range(len(palavra_selecionada)):
-                if palavra_selecionada[i]==letra:
-                    letras[i] = letra
+        letras = [" _ "] * len(palavra_selecionada)
+    
+        letras_usadas = [ ]
+    
+        erros = 0
+    
+        max_erros= 6
+    
+        while " _ " in letras and erros < max_erros:
+            print(" ".join(letras))
+    
+            letra=input("Introduza uma letra:")
+    
+            if letra in letras_usadas:
+                print("Letra já utilizada")
+                continue
+            letras_usadas.append(letra)
+            print(letras_usadas)
+    
+            if letra in palavra_selecionada:
+                for i in range(len(palavra_selecionada)):
+                    if palavra_selecionada[i]==letra:
+                        letras[i] = letra
                 
+            else:
+                print("Letra errada")
+                erros += 1
+        if " _ " not in letras:
+            print(f"Vitória!! A palavra era: {palavra_selecionada}")
         else:
-            print("Letra errada")
-            erros += 1
-    if " _ " not in letras:
-        print(f"Vitória!! A palavra era: {palavra_selecionada}")
-    else:
-        print(f"Derrota!! A palavra era: {palavra_selecionada}")
-    jogar= input("Queres jogar novamente? (Escreva jogar para continuar a jogar): ")
+            print(f"Derrota!! A palavra era: {palavra_selecionada}")
+        jogar= input("Queres jogar novamente? (Escreva jogar para continuar a jogar): ")
 
-    print("Obrigado por jogares")
+        print("Obrigado por jogares")
 
