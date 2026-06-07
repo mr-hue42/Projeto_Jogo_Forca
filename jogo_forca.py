80f42d383fe83160b3fe756ea23c60352a0fe3c1
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 19:51:00 +0100

    Letra usada
    Confirmar se a letra indicada já foi utilizada

--- jogo_forca.py
+++ jogo_forca.py
@@ -17,10 +17,18 @@ palavra_selecionada = random.choice(palavras).lower()
 
 letras = [" _ "] * len(palavra_selecionada)
 
+letras_usadas = [ ]
+
 while " _ " in letras:
     print(" ".join(letras))
     
     letra=input("Introduza uma letra:")
+    
+    if letra in letras_usadas:
+        print("Letra já utilizada")
+        continue
+    letras_usadas.append(letra)
+    print(letras_usadas)
     
     if letra in palavra_selecionada:
         for i in range(len(palavra_selecionada)):
