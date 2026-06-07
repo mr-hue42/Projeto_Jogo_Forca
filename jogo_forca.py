f7f81406381748fc61e8fa794404f5208691219c
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-07 19:44:33 +0100

    Introdução do jogo
    Descrição e apresentação das regras do jogo

--- jogo_forca.py
+++ jogo_forca.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  6 07:15:22 2026
+print("Bem vindo(a) ao jogo da forca")
+print()
+print("Regras:")
+print()
+print("-Cada traço representa uma letra da palavra secreta")
+print("-Em cada tentiva deverá indicar uma letra")
+print("-O número máximo de tentativas erradas é igual a 6")
+print("-Vitória: Descobrir a palavra secreta antes de atingir o nº máximo de erros")
+print("-Derrota: Atingir o nº máximo de erros (6) antes de descobrir a palavra secreta")
 
-@author: filip
-"""
+
 
